a26f3cfd48f141395b619ad785bcf2ca210e7553
Albert Siddhartha Slawinski <errendir@gmail.com>
2017-10-16 18:30:15 +0200

    Add docs

--- rollup.example.config.js
+++ rollup.example.config.js
@@ -2,7 +2,7 @@ import createRollupConfig from './base.rollup.config'
 
 export default createRollupConfig({ 
   inputFile: "./src/example.tsx",
-  outputFile: "dist/example-umd.js",
+  outputFile: "docs/example-umd.js",
   outputFormat: "iife",
   bundleReact: false
 })
